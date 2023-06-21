68b0a68700fa42be6a1b133ca9f5d06ac06f61d1
FelixNiklas <63049634+FelixNiklas@users.noreply.github.com>
2023-06-21 12:55:35 +0200

    Grid layout with calculator base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,299 @@
-import Image from 'next/image'
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  Button,
+  Typography,
+  Box,
+  Select,
+  MenuItem,
+} from '@mui/material';
+import styles from './savings.module.scss';
 
 export default function Home() {
+  const channels = [
+    { id: 'linkedin', label: 'LinkedIn' },
+    { id: 'instagram', label: 'Instagram' },
+    { id: 'facebook', label: 'Facebook' },
+    { id: 'tiktok', label: 'TikTok' },
+    { id: 'google-ads', label: 'Google Ads' },
+    { id: 'sales-messages', label: 'Sales Messages' },
+    { id: 'interne commmunicatie', label: 'interne communicatie' },
+  ];
+
+  const [selectedChannels, setSelectedChannels] = useState<string[]>([
+    'linkedin',
+  ]);
+  const [postsPerWeek, setPostsPerWeek] = useState<number>(3);
+  const [countries, setCountries] = useState<number>(1);
+  const [timeSpentPerPost, setTimeSpentPerPost] = useState<number>(1);
+  const [hourlyRate, setHourlyRate] = useState<number>(60);
+  const [totalCost, setTotalCost] = useState<number>(0);
+  const [estimatedSavings, setEstimatedSavings] = useState<number>(0);
+  const [timeInterval, setTimeInterval] = useState<string>('jaar');
+  const savingPercentage = 90;
+
+  useEffect(() => {
+    calculateSavings();
+  }, [
+    selectedChannels,
+    postsPerWeek,
+    countries,
+    timeSpentPerPost,
+    hourlyRate,
+    timeInterval,
+  ]);
+
+  const handleChannelChange = (e: { target: { value: any } }) => {
+    const selectedChannels = e.target.value;
+    setSelectedChannels(selectedChannels);
+  };
+
+  const handleCalculate = () => {
+    calculateSavings();
+  };
+
+  function formatMoney(amount: number) {
+    return amount.toLocaleString('nl-NL');
+  }
+
+  const calculateSavings = () => {
+    const totalPosts = postsPerWeek * 52 * selectedChannels.length;
+    const totalHoursSaved = totalPosts * timeSpentPerPost;
+    const totalCost = totalHoursSaved * hourlyRate;
+    const totalSavings = totalCost * (savingPercentage / 100);
+    let totalCostPerInterval = 0;
+    let savingsPerInterval = 0;
+
+    switch (timeInterval) {
+      case 'dag':
+        totalCostPerInterval = totalCost / 365;
+        savingsPerInterval = totalSavings / 365;
+        break;
+      case 'week':
+        totalCostPerInterval = totalCost / 52;
+        savingsPerInterval = totalSavings / 52;
+
+        break;
+      case 'maand':
+        totalCostPerInterval = totalCost / 12;
+        savingsPerInterval = totalSavings / 12;
+        break;
+      case 'jaar':
+        totalCostPerInterval = totalCost;
+        savingsPerInterval = totalSavings;
+        break;
+      default:
+        savingsPerInterval = 0;
+    }
+
+    setEstimatedSavings(savingsPerInterval);
+    setTotalCost(totalCostPerInterval);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div id={styles.savingsCalculator}>
+      <div className={styles.gridContainer}>
+        <div className={styles.topRow}>
+          <Typography variant="h4">Savings Calculator</Typography>
+          <Typography variant="h6">
+            Wat we voor je kunnen betekenen uitgedrukt in cijfers
+          </Typography>
+        </div>
+        <div className={styles.middleRow}>
+          <div className={styles.leftColumn}>
+            {' '}
+            <Box>
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">Channels Used</Typography>
+                {JSON.stringify(selectedChannels)}
+                <FormControl component="fieldset">
+                  <RadioGroup
+                    value={selectedChannels}
+                    onChange={handleChannelChange}
+                    style={{
+                      display: 'grid',
+                      gridTemplateColumns: 'repeat(2, 1fr)',
+                      gridGap: '10px',
+                    }}
+                  >
+                    {channels.map((channel) => (
+                      <FormControlLabel
+                        key={channel.id}
+                        value={channel.id}
+                        control={<Radio />}
+                        label={channel.label}
+                      />
+                    ))}
+                  </RadioGroup>
+                </FormControl>
+              </Box>
+
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">
+                  Posts per Week per Channel
+                </Typography>
+                <FormControl fullWidth>
+                  <TextField
+                    type="number"
+                    inputProps={{ min: 1, max: 21 }}
+                    value={postsPerWeek}
+                    onChange={(e: { target: { value: string } }) =>
+                      setPostsPerWeek(parseInt(e.target.value))
+                    }
+                  />
+                </FormControl>
+              </Box>
+
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">Countries</Typography>
+                <FormControl fullWidth>
+                  <TextField
+                    type="number"
+                    inputProps={{ min: 1, max: 21 }}
+                    value={countries}
+                    onChange={(e: { target: { value: string } }) =>
+                      setCountries(parseInt(e.target.value))
+                    }
+                  />
+                </FormControl>
+              </Box>
+            </Box>
+          </div>
+          <div className={styles.centerColumn}>
+            {' '}
+            <Box>
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">
+                  Time Spent per Post (in hours)
+                </Typography>
+                <FormControl fullWidth>
+                  <TextField
+                    type="number"
+                    inputProps={{ min: 0 }}
+                    value={timeSpentPerPost}
+                    onChange={(e: { target: { value: string } }) =>
+                      setTimeSpentPerPost(parseInt(e.target.value))
+                    }
+                  />
+                </FormControl>
+              </Box>
+
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">
+                  Hourly Rate of the Employee (in euros)
+                </Typography>
+                <FormControl fullWidth>
+                  <TextField
+                    type="number"
+                    inputProps={{ min: 0 }}
+                    value={hourlyRate}
+                    onChange={(e: { target: { value: string } }) =>
+                      setHourlyRate(parseInt(e.target.value))
+                    }
+                  />
+                </FormControl>
+              </Box>
+
+              <Box sx={{ width: 200, marginBottom: 2 }}>
+                <Typography variant="subtitle1">Time Interval</Typography>
+                <FormControl fullWidth>
+                  <Select
+                    value={timeInterval}
+                    onChange={(e: {
+                      target: { value: React.SetStateAction<string> };
+                    }) => setTimeInterval(e.target.value)}
+                  >
+                    <MenuItem value="dag">Dag</MenuItem>
+                    <MenuItem value="week">Week</MenuItem>
+                    <MenuItem value="maand">Maand</MenuItem>
+                    <MenuItem value="jaar">Jaar</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+
+              <Typography variant="h6">
+                <span style={{ color: 'red', textDecoration: 'line-through' }}>
+                  €{formatMoney(totalCost)}
+                </span>{' '}
+                per {timeInterval} !
+              </Typography>
+
+              <Typography variant="h6">
+                * Met Stencil bespaar jij tot{' '}
+                <span style={{ color: 'green' }}>
+                  €{formatMoney(estimatedSavings)}
+                </span>{' '}
+                per {timeInterval} !
+              </Typography>
+              <Button
+                variant="contained"
+                onClick={handleCalculate}
+                sx={{ marginBottom: 2 }}
+              >
+                Begin met besparen!
+              </Button>
+            </Box>
+          </div>
+          <div className={styles.rightColumn}>
+            <Typography variant="h5" sx={{ marginBottom: '16px' }}>
+              Andere voordelen
+            </Typography>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
+              <li>
+                <span role="img" aria-label="checkmark">
+                  ✅
+                </span>
+                <Typography variant="body1" sx={{ marginLeft: '8px' }}>
+                  Geen huisstijlpolitie meer nodig
+                </Typography>
+              </li>
+              <li>
+                <span role="img" aria-label="checkmark">
+                  ✅
+                </span>
+                <Typography variant="body1" sx={{ marginLeft: '8px' }}>
+                  Meer tijd voor
+                </Typography>
+              </li>
+              <li>
+                <span role="img" aria-label="checkmark">
+                  ✅
+                </span>
+                <Typography variant="body1" sx={{ marginLeft: '8px' }}>
+                  Schaalbaar
+                </Typography>
+              </li>
+              <li>
+                <span role="img" aria-label="checkmark">
+                  ✅
+                </span>
+                <Typography variant="body1" sx={{ marginLeft: '8px' }}>
+                  Kortere doorlooptijd
+                </Typography>
+              </li>
+              <li>
+                <span role="img" aria-label="checkmark">
+                  ✅
+                </span>
+                <Typography variant="body1" sx={{ marginLeft: '8px' }}>
+                  Zelf in controle
+                </Typography>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.bottomRow}>
+          * aan dit rekenvoorbeeld kunnen geen rechten worden ontleend, <br /> s
+          <a>neem contact met ons op</a> voor een <a>offerte</a> of{' '}
+          <a>vrijblijvend adviesgesprek.</a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
